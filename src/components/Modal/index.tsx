5e967938eddf3bea3b04ba19730d60133ba925ff
Karine Rafaele <karinerafaele@pop-os.localdomain>
2022-05-02 19:08:42 -0300

    Changed all the class components to the functional components in the project

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,30 +1,20 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+interface ModalProps {
+  isOpen: boolean;  
+  setIsOpen: () => void;
+  children: React.ReactNode
+}
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
+export default function Modal ({isOpen, setIsOpen, children}: ModalProps) {
+  const [modalStatus, setModalStatus] = useState(isOpen);
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
-
-    return (
+  useEffect(() => {
+    setModalStatus(isOpen)
+  },[isOpen])
+  
+  return (
       <ReactModal
         shouldCloseOnOverlayClick={!false}
         onRequestClose={setIsOpen}
@@ -53,6 +43,3 @@ class Modal extends Component {
       </ReactModal>
     );
   }
-};
-
-export default Modal;
